eaaeb1e65451942181199d14cca4f93105fb4438
John Ambrosiano <ambro@lanl.gov>
2016-10-11 18:27:43 -0600

    Qt developement example begun.

---
+++ examples/mobileAppExample/mobileapp/model.py
@@ -0,0 +1,16 @@
+class MobileApp:
+	maker = "unknown"
+	category = "unknown"
+	os = "unknown"
+	price = 0.0
+	def __init__(self,name):
+		self.name = name
+class AppCategory:
+	game = "game"
+	productivity = "productivity"
+	audio ="audio"
+	video = "video"
+class OSCategory:
+	ios = "ios"
+	android = "android"
+
