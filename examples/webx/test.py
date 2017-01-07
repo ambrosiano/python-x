0feba2ac9c5f2cce5f032124c9a8e1924b3024e6
ambrosiano <ambro@lanl.gov>
2017-01-07 11:56:33 -0700

    urllib/bs4 example updated.

--- examples/webx/test.py
+++ examples/webx/test.py
@@ -4,4 +4,5 @@ from urllib2 import *
 if __name__ == '__main__':
     print('test')
     html = urlopen("http://pythonscraping.com/pages/page1.html")
-    print(html.read())
+    bshtml = BeautifulSoup(html.read(),'lxml')
+    print(bshtml.div)
